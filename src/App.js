7fad7f6fa326e317214b9630290d32ae1ad0a22c
Karim <karim@yandex.ru>
2024-03-28 19:40:57 +0300

    test

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   return (
     <div className="App">
-      <button onClick={onToggleButton}></button>
+      <button onClick={onToggleButton}>toggle</button>
     </div>
   );
 }
