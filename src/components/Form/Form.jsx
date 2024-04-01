11e6889569fbc366299f6e5a66f7b1a4df9e3b67
Karim <karim@yandex.ru>
2024-04-02 00:51:05 +0300

    test

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import  "./Form.css"
 import {useTelegram} from "../../hooks/useTelegram";
 const Form = () => {
@@ -7,6 +7,27 @@ const Form = () => {
     const [subject, setSubject] = useState('physical')
 
     const {tg} = useTelegram()
+
+
+    const onSendData = useCallback(() => {
+        const data = {
+            country,
+            street,
+            subject
+        }
+
+        tg.sendData(JSON.stringify(data))
+    }, [country,
+        street,
+        subject])
+
+    useEffect(() => {
+        tg.onEvent('mainButtonClicked', onSendData)
+
+        return () => {
+            tg.offEvent('mainButtonClicked', onSendData)
+        }
+    }, [])
 
     useEffect(() => {
         tg.MainButton.setParams({
@@ -39,7 +60,6 @@ const Form = () => {
                 <option value={'physical'}>Физ. лицо</option>
                 <option value={'legal'}>Юр. лицо</option>
             </select>
-            Form
         </div>
     );
 };
