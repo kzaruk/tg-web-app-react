1c0ffad457980c70039f7a57b71c933d112d5a41
Karim <karim@yandex.ru>
2024-04-05 19:48:41 +0300

    test

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import {Route, Routes} from "react-router-dom";
 import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
-const BASE_URL = 'https://steady-parfait-5284bb.netlify.app'
 function App() {
   const {tg, onToggleButton} = useTelegram()
 
@@ -18,8 +17,8 @@ function App() {
     <div className="App">
         <Header/>
         <Routes>
-            <Route path={BASE_URL} element={<ProductList/>}/>
-            <Route path={BASE_URL +'/form'} element={<Form/>}/>
+            <Route index element={<ProductList/>}/>
+            <Route path={'form'} element={<Form/>}/>
         </Routes>
       {/*<button onClick={onToggleButton}>toggle</button>*/}
     </div>
